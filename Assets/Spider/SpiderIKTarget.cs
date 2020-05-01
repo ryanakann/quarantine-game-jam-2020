8cab00fb0b1c255a16ddbad679c7a44fe3c0b645
ryanakann <ryanakann@gmail.com>
2020-05-01 19:29:14 -0400

    Stipple Shader

---
+++ Assets/Spider/SpiderIKTarget.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderIKTarget : MonoBehaviour {
+    public LayerMask groundMask;
+
+    RaycastHit hit;
+
+    void Update () {
+        //if (Physics.Raycast())    
+    }
+}
