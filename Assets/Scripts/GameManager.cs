d9df6ff955f79c2643714867d7028b936e984a44
Wyatt Joyner <wyatt.joyner@practicalcode.us>
2020-05-03 15:16:50 -0400

    toot?

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Singleton code
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
